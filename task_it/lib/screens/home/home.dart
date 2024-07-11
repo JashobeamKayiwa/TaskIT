a6b05ce955d5e0ef5259e8059e73bb958764455f
Baraka Maurice <nickbaraka96@gmail.com>
2024-07-11 14:27:11 +0300

    appBar Homepage

--- task_it/lib/screens/home/home.dart
+++ task_it/lib/screens/home/home.dart
@@ -1,2 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0), // Adjust the height as needed
+        child: _buildAppBar(),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+        )
+    );
+  }
+}
+
+Widget _buildAppBar() {
+  return Material(
+    elevation: 5,
+    borderRadius: BorderRadius.only(
+      bottomLeft: Radius.circular(30.0),
+      bottomRight: Radius.circular(30.0),
+    ),
+    shadowColor: Colors.grey.withOpacity(0.5),
+    child: ClipRRect(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(20.0),
+        bottomRight: Radius.circular(20.0),
+      ),
+      child: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            Text('Welcome Michelle',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
+        ),
+        actions: [Icon(Icons.add_alert_rounded, color: Colors.black, size: 40)],
+      ),
+    ),
+  );
+}
