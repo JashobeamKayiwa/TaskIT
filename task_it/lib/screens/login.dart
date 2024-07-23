3d552d49d7cbc9a51ad72de19c069c8ed7ea4219
Baraka Maurice <nickbaraka96@gmail.com>
2024-07-23 13:12:47 +0300

    login Page

--- task_it/lib/screens/login.dart
+++ task_it/lib/screens/login.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  width: 270,
+                  height: 310,
+                  child: Image(
+                    image: AssetImage('assets/images/taskit.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+              child: Divider(
+                color: Colors.black,
+                thickness: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
