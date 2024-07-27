fc713962ecd4cbc8d825daaadbd8a821c7527744
BarakaM10 <nickbaraka96@gmail.com>
2024-07-27 12:23:09 +0300

    otp input UI

--- task_it/lib/screens/forgot.dart
+++ task_it/lib/screens/forgot.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:task_it/constants/colors.dart';
 
 class ForgotPage extends StatelessWidget {
   const ForgotPage({super.key});
@@ -7,7 +9,6 @@ class ForgotPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
         height: double.infinity,
         padding: EdgeInsets.all(20.0),
         child: Column(
@@ -20,6 +21,31 @@ class ForgotPage extends StatelessWidget {
                     "CO\nDE",
                     textAlign: TextAlign.center,
                     style: TextStyle(fontSize: 80.0),
+                  ),
+                  Text(
+                    "Please enter verification code",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  OtpTextField(
+                    numberOfFields: 7,
+                    fillColor: const Color.fromARGB(255, 211, 209, 209),
+                    filled: true,
+                    onSubmit: (code) {
+                      print("Code is $code");
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  SizedBox(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Next", style: TextStyle(color: kWhite)),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: kBlack,
+                          padding: EdgeInsets.only(left: 100.0, right: 100.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    ),
                   ),
                 ],
               ),
