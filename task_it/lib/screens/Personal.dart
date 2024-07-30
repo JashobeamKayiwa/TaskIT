0c27858dd0945a3ce3af8af42a0e7d0a7bcb0fa8
Brightsamuel <brightsamuel344@gmail.com>
2024-07-30 13:06:58 +0300

    "Modified persoanl, added multiple files"

--- task_it/lib/screens/Personal.dart
+++ task_it/lib/screens/Personal.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:task_it/views/add_view.dart';
-import 'package:task_it/views/header.dart';
-import 'package:task_it/views/task_info.dart';
-import 'package:task_it/views/task_list.dart';
+import 'package:provider/provider.dart';
+import 'package:task_it/view_models/app_view.dart';
+import 'package:task_it/screens/add_view.dart';
+import 'package:task_it/screens/header.dart';
+import 'package:task_it/screens/task_info.dart';
+import 'package:task_it/screens/task_list.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+      create: (context) => personal(), child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TaskPage(),
+    );
+  }
+}
+
 
 class TaskPage extends StatelessWidget {
   const TaskPage({super.key});
