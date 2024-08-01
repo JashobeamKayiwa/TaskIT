7ae3e4a7d39da4090bc7d58f62e3c4379d5d27d1
BarakaM10 <nickbaraka96@gmail.com>
2024-08-01 13:28:00 +0300

    compatibility fixes

--- task_it/lib/main.dart
+++ task_it/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:task_it/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:task_it/screens/login.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+   Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -13,6 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TaskIT',
-       );
+      home: LoginPage(),
+    );
   }
 }
