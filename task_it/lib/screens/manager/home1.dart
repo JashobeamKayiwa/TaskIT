b8702dd91ec1720b54e94687c2161e1ae636e5d8
Your Name <you@example.com>
2024-08-10 16:02:16 +0300

    implement profile.dart

--- task_it/lib/screens/manager/home1.dart
+++ task_it/lib/screens/manager/home1.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
 import 'package:task_it/constants/colors.dart';
 import 'package:task_it/screens/manager/home.dart';
+import 'package:task_it/screens/manager/profile.dart';
 import 'package:task_it/screens/manager/statistics.dart';
 
 class Home extends StatefulWidget {
@@ -13,8 +14,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? user;
   int selectedindex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    user = _auth.currentUser;
+  }
 
   void navigateBottomBar(int index) {
     setState(() {
@@ -22,53 +30,74 @@ class _HomeState extends State<Home> {
     });
   }
 
-  List pages = [
-    HomePage(),
-    Statistics(),
-    //Profile(),
-  ];
+  List<Widget> get pages {
+    if (user == null) {
+      return [
+         HomePage(),
+         Statistics(),
+         UserProfile(
+          userId: '', // Provide an empty string or handle it accordingly.
+        ),
+      ];
+    }
+    return [
+      const HomePage(),
+      const Statistics(),
+      UserProfile(
+        userId: user!.uid, // Safe to access `uid` since `user` is non-null.
+      ),
+    ];
+  }
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: pages[selectedindex],
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 10,
             ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 5,
-                blurRadius: 10,
-              )
-            ]),
+          ],
+        ),
         child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
-            child: BottomNavigationBar(
-              backgroundColor: Colors.white,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              selectedItemColor: kBlack,
-              unselectedItemColor: Colors.grey.withOpacity(0.5),
-              onTap: navigateBottomBar,
-              items: const [
-                BottomNavigationBarItem(
-                    label: 'Home', icon: Icon(Icons.home_rounded, size: 40)),
-                BottomNavigationBarItem(
-                    label: 'Finances', icon: Icon(Icons.bar_chart, size: 40)),
-                BottomNavigationBarItem(
-                    label: 'Person',
-                    icon: Icon(Icons.person_rounded, size: 40)),
-              ],
-            ))),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            selectedItemColor: kBlack,
+            unselectedItemColor: Colors.grey.withOpacity(0.5),
+            onTap: navigateBottomBar,
+            currentIndex: selectedindex, // Keep track of the current index.
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(Icons.home_rounded, size: 40),
+              ),
+              BottomNavigationBarItem(
+                label: 'Finances',
+                icon: Icon(Icons.bar_chart, size: 40),
+              ),
+              BottomNavigationBarItem(
+                label: 'Person',
+                icon: Icon(Icons.person_rounded, size: 40),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- task_it/lib/screens/manager/profile.dart
+++ task_it/lib/screens/manager/profile.dart
@@ -8,7 +8,8 @@ class UserProfile extends StatelessWidget {
   UserProfile({required this.userId});
 
   Future<Map<String, dynamic>?> getUserDetails(String userId) async {
-    DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    DocumentSnapshot snapshot =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
     if (snapshot.exists) {
       return snapshot.data() as Map<String, dynamic>;
     }
@@ -18,35 +19,69 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
-      ),
-      body: FutureBuilder<Map<String, dynamic>?>(
-        future: getUserDetails(userId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data == null) {
-            return Center(child: Text('User not found'));
-          } else {
-            var userData = snapshot.data!;
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: Material(
+          elevation: 0,
+          child: ClipRRect(
+            child: AppBar(
+              backgroundColor: Colors.black,
+              elevation: 0,
+              title: Row(
                 children: [
-                  Text('Name: ${userData['name']}', style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 8),
-                  Text('Phone Number: ${userData['phoneNumber']}', style: TextStyle(fontSize: 20)),
-                  SizedBox(height: 8),
-                  Text('Email: ${userData['email']}', style: TextStyle(fontSize: 20)),
+                  Text(
+                    'User Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ],
               ),
-            );
-          }
-        },
+            ),
+          ),
+        ),
+      ),
+      body: SizedBox.expand(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            color: Colors.white,
+          ),
+          child: FutureBuilder<Map<String, dynamic>?>(
+            future: getUserDetails(userId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData || snapshot.data == null) {
+                return Center(child: Text('User not found'));
+              } else {
+                var userData = snapshot.data!;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Name: ${userData['name']}',
+                        style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 8),
+                    Text('Phone Number: ${userData['phoneNumber']}',
+                        style: TextStyle(fontSize: 20)),
+                    SizedBox(height: 8),
+                    Text('Email: ${userData['email']}',
+                        style: TextStyle(fontSize: 20)),
+                  ],
+                );
+              }
+            },
+          ),
+        ),
       ),
     );
   }
