310a21f9d32e3aafc4dda10e08f951f1b0a9785c
jordanmwesigwa <jordanssemuyimba@gmail.com>
2024-08-11 17:48:24 +0300

    app bar widget and bottom navigation bar

--- task_it/lib/screens/manager/personal.dart
+++ task_it/lib/screens/manager/personal.dart
@@ -136,3 +136,84 @@ class Personal extends StatelessWidget {
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
+
+Widget _buildAppBar(BuildContext context) {
+    return Material(
+      elevation: 0,
+      child: ClipRRect(
+        child: AppBar(
+          backgroundColor: kBlack,
+          elevation: 0,
+          title: Row(
+            children: [
+              Text(
+                'PERSONAL',
+                style: TextStyle(
+                  color: kWhite,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add, color: kWhite, size: 40),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyTask()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 5,
+            blurRadius: 10,
+          )
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: kBlack,
+          unselectedItemColor: Colors.grey.withOpacity(0.5),
+          items: [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home_rounded, size: 40),
+            ),
+            BottomNavigationBarItem(
+              label: 'Finances',
+              icon: Icon(Icons.attach_money_outlined, size: 40),
+            ),
+            BottomNavigationBarItem(
+              label: 'Person',
+              icon: Icon(Icons.person_rounded, size: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
