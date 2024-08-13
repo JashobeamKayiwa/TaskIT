e1f6c761f7437538f1a6f15ca80e1a9fd9ae70b7
Your Name <you@example.com>
2024-08-13 16:35:49 +0300

    merge

--- task_it/lib/requirements/task.dart
+++
@@ -1,8 +0,0 @@
-class Task {
-  String title;
-  String deadline;
-  String assigned_to;
-  bool isCompleted;
-
-  Task({required this.title, required this.deadline, required this.assigned_to,this.isCompleted = false});
-}
